5ef17b5e10c37ed14237aa3d6ea37dd5a221bae1
Mishthi <mishjain02@gmail.com>
2025-09-13 23:22:59 +0530

    edited dashboard layout

--- apps/web/modules/auth/ui/components/auth-guard.tsx
+++ apps/web/modules/auth/ui/components/auth-guard.tsx
@@ -1,4 +1,6 @@
 "use client"
+import React from "react";
+
  import { Authenticated,Unauthenticated,AuthLoading } from "convex/react"
  import { AuthLayout } from "../layouts/auth-layout"
  import { SignInView } from "../views/sign-in-view"
--- apps/web/modules/auth/ui/components/organization-guard.tsx
+++ apps/web/modules/auth/ui/components/organization-guard.tsx
@@ -1,6 +1,5 @@
 "use client"
 import { useOrganization } from "@clerk/nextjs"
-import { AuthLayout } from "../layouts/auth-layout"
 import { OrgSelectView } from "../views/org-select-view";
 export const OrganizationGuard=({children}:{children:React.ReactNode;})=>{
     const {organization}=useOrganization()
