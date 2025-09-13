ef8b3d5d41e81f49854a85fd2c72c857e6f6ef39
Mishthi <mishjain02@gmail.com>
2025-09-13 16:18:17 +0530

    added organizations

--- apps/web/app/(dashboard)/page.tsx
+++ apps/web/app/(dashboard)/page.tsx
@@ -1,26 +1,19 @@
 "use client";
-import { Authenticated, Unauthenticated } from "convex/react";
 import {useMutation, useQuery} from "convex/react"
 import {api} from "@workspace/backend/_generated/api"
 import {Button} from "@workspace/ui/components/button"
-import { SignInButton, UserButton } from "@clerk/nextjs";
+import { OrganizationSwitcher, SignInButton, UserButton } from "@clerk/nextjs";
 export default function Page() {
   const users=useQuery(api.users.getMany);
   const addUser=useMutation(api.users.add)
   return (
     <>
-    <Authenticated>
     <div className="flex items-center justify-center min-h-svh">
       <UserButton></UserButton>
+      <OrganizationSwitcher hidePersonal></OrganizationSwitcher>
       <Button onClick={()=> addUser()}>Add</Button>
       <p>web/app</p>
     </div>
-    </Authenticated>
-    <Unauthenticated>
-      <p>Must be signed in!</p>
-
-      <SignInButton>Sign In</SignInButton>
-      </Unauthenticated>
     </>
   )
 }
---
+++ apps/web/modules/auth/ui/components/organization-guard.tsx
@@ -0,0 +1,19 @@
+"use client"
+import { useOrganization } from "@clerk/nextjs"
+import { AuthLayout } from "../layouts/auth-layout"
+import { OrgSelectView } from "../views/org-select-view";
+export const OrganizationGuard=({children}:{children:React.ReactNode;})=>{
+    const {organization}=useOrganization()
+    if(!organization){
+        return(
+            <div>
+                <OrgSelectView></OrgSelectView>
+            </div>
+        )
+    }
+    return(
+        <div>
+            {children}
+        </div>
+    )
+}
