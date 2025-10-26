064abced0da8d7ff0824cb6656be12a456a6a0e8
Mishthi <mishjain02@gmail.com>
2025-10-26 15:24:58 +0530

    added ai search tool

--- packages/backend/convex/public/messages.ts
+++ packages/backend/convex/public/messages.ts
@@ -6,6 +6,7 @@ import { paginationOptsValidator } from "convex/server";
 import { resolveConversation } from "../system/ai/tools/resolveConversation";
 import { escalateConversation } from "../system/ai/tools/escalateConversation";
 import { saveMessage } from "@convex-dev/agent";
+import { search } from "../system/ai/tools/search";
 
 export const create=action({
     args:{
@@ -54,7 +55,8 @@ export const create=action({
                     prompt:args.prompt,
                     tools:{
                         resolveConversation,
-                        escalateConversation
+                        escalateConversation,
+                        search
                     }
                 }
             )
--- packages/backend/convex/system/ai/agents/supportAgent.ts
+++ packages/backend/convex/system/ai/agents/supportAgent.ts
@@ -1,10 +1,11 @@
 import { google } from '@ai-sdk/google';
 import { Agent } from "@convex-dev/agent";
 import {  components } from "../../../_generated/api";
+import { SUPPORT_AGENT_PROMPT } from '../constants';
 
 export const supportAgent = new Agent(components.agent, {
   name: "support-agent",
   chat: google("models/gemini-1.5-pro"),
-  instructions: `You are a customer support agent. Use "resolveConversation" tool when user expresses finalization of the conversation. Use "escalateConversation" tool when user expresses frustation, or requests a human explicitly. `,
+  instructions: SUPPORT_AGENT_PROMPT,
   
 });
